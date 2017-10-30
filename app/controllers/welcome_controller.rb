18358febf053acd7e2a97a60c334e24dff88b091
Uziel Trujillo <uziel.trujillo@gmail.com>
2017-10-30 14:46:20 -0600

    [#152399758] Se agrega a el template welcome el apartado de los lugares
    Nota:
    - Se carga una imagen SVG que se agregara como vista previa
    	new file:   app/assets/images/imageCap.svg
    	modified:   app/assets/stylesheets/application.scss
    	modified:   app/assets/stylesheets/welcome.scss
    	new file:   app/assets/stylesheets/welcome/_places.scss
    	modified:   app/controllers/welcome_controller.rb
    	modified:   app/views/welcome/index.html.erb
    	new file:   app/views/welcome/sections/_places.html.erb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
 
   def index
     @imageSlider = Dir.glob("#{Rails.root}/app/assets/images/slider/*.jpg")
+    @places = ['Bares', 'Playas', 'Cines', 'Restaurantes', 'Gimnasios', 'Tiendas', 'Deportes', 'Oficinas']
   end
 
 end
